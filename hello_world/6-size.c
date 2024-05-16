05bf93b1b802e9222ecff1e18e6210003fb94b70
clay <rpctropic13@gmail.com>
2024-05-16 18:24:37 -0500

    data types

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- *Prints the different types of file types by there size.
+ *Main - Prints the different types of file types by there size.
  *Returns 0
 */
 int main(void);{
-printf("Size of a char: %c byte(s)
-printf("Size of an int: %d byte(s)
-printf("Size of a long int: %ld byte(s)
-printf("Size of a long long int: %ld byte(s)
-printf("Size of a float: %d byte(s)
+printf("Size of a char: %c byte(s)", sizeof(char));
+printf("Size of an int: %d byte(s)", sizeof(int));
+printf("Size of a long int: %ld byte(s)", sizeof(longint));
+printf("Size of a long long int: %ld byte(s)", sizeof(llint));
+printf("Size of a float: %d byte(s)", sizeof(float));
 return (0);
 }
