7eca81bf503fbda4297d2ba96b96a869c8a2c243
clay <rpctropic13@gmail.com>
2024-05-16 17:30:58 -0500

    art

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - something to do with art.
+ *
+ *this script prints a single link.
+ *
+ *Return: 0
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
