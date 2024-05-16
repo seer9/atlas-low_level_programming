641ae5a5e3c3cfe3681b9d3a8af7df408be3cc29
root <rpctropic13@gmail.com>
2024-05-16 23:53:39 +0000

    Data Types

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,11 +6,10 @@
  */
 int main(void)
 {
-printf("Size of a char: %c byte(s)", sizeof(char));
-printf("Size of an int: %d byte(s)", sizeof(int));
-printf("Size of a long int: %ld byte(s)", sizeof(long int));
-printf("Size of a long long int: %ld byte(s)", sizeof(long long int));
-printf("Size of a float: %ld byte(s)", sizeof(float));
+	printf("Size of a char: %c byte(s)", sizeof(char));
+	printf("Size of an int: %d byte(s)", sizeof(int));
+	printf("Size of a long int: %ld byte(s)", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)", sizeof(float));
 return (0);
 }
-
