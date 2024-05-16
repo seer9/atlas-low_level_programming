fdf8864475c5633f01109f0fcf0bfb45115de988
clay <rpctropic13@gmail.com>
2024-05-16 14:27:48 -0500

    puzzle

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #/bin/bash
-gcc -o 4-puts_0 -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c
 
-int puzzle(void)
+int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");
+    puts("\"Programming is like building a multilingual puzzle");
     
 return (0);
 }
