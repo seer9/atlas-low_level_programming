7a004befd6fc9463541962f8208c8fd000531f9b
clay <rpctropic13@gmail.com>
2024-05-16 18:32:54 -0500

    data types

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -3,7 +3,7 @@
  *Main - Prints the different types of file types by there size.
  *Returns 0
 */
-int main(void);{
+int main(void); {
 printf("Size of a char: %c byte(s)", sizeof(char));
 printf("Size of an int: %d byte(s)", sizeof(int));
 printf("Size of a long int: %ld byte(s)", sizeof(longint));
