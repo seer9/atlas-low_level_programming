f815c91fee6a9f68fa07e6d641426fce67af24fc
clay <rpctropic13@gmail.com>
2024-05-16 15:03:10 -0500

    puzzle

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-
-/**
- *this script prnts a single link.
- *
- * function named main  prints "Programming is like building a multilingual
- *     puzzle"
- *
- * Return: 0
- */
+/*
+ this script prnts a single link.
+ 
+  function named main  prints "Programming is like building a multilingual
+      puzzle"
+ 
+  Return: 0
+*/
 int main(void)
 {
     puts("\"Programming is like building a multilingual puzzle");
