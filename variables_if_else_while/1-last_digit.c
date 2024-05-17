c1e00942a722d034918859710745d343989956cb
root <rpctropic13@gmail.com>
2024-05-17 04:11:56 +0000

    rand

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -5,22 +5,21 @@
  *main - assigns a random number to n.
  *divide the random number by 10 to find the last number then finding the number.
  */
+
 int main(void)
 {
-`int n;
- int last;
-
+	int n;
+ 	int last;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	last = n % 10;
-	/* your code goes there */
+	
+	last = (n % 10);
 		printf("Last digit of %d is %d ", n, last);
 	if (last % 5)
 		printf("and is greater than 5\n");
 	if (last < 6 && last != 0)
 		printf("and is less than 6 and not 0\n");
-	if (z == 0)
+	else
 		printf("and is 0\n");
-
 	return (0);
 }
