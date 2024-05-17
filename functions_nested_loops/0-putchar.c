1a1b73c3a7cf5d2cae3f3ed68412332819d9b0c6
root <rpctropic13@gmail.com>
2024-05-17 22:34:49 +0000

    ole

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print _putchar followed by a new line.
+ * Return: 0
+ */
+int main(void)
+{
+	int ole;
+
+	for (ole = 0; ole < 1; ole++)
+		printf("_putchar\n");
+	return (0);
+}
