375e53a8aedd4fb9435f02a7a57631586611c102
root <rpctropic13@gmail.com>
2024-05-17 14:45:06 +0000

    abc

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - var named 'i' is used to interate through the alphabet.
+ * Return: 0
+ */
+
+int main(void)
+{
+	char i;
+	for (i = 'a'; i <= 'z'; i++);
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
