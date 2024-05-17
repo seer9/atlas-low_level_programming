3a671dc0243304456b43987f47af7d0029a4e861
root <rpctropic13@gmail.com>
2024-05-17 15:32:45 +0000

    noEnoQ

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - this is going to go through the alphabet but skip 'e' and 'q'
+ * Return: 0
+ */
+
+int main(void)
+{
+	char i;
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			if (i != 'e' & i != 'q')
+				putchar(i);
+		}
+		putchar("\n");
+		return (0);
+}
