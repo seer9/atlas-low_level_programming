cc5e0930b1cfe80d549b9560e7d8393cb4b6c524
clay <rpctropic13@gmail.com>
2024-05-16 21:11:21 -0500

    README.md

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -7,11 +7,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if n > 0;
+	if (n > 0);
 		printf("%d is positive\n");
-	else n < 0;
+	else (n < 0);
 		printf("%d is negaative\n");
-	else if n ==0;
+	else if (n ==0);
 		printf("%d is zero\n");
 
 
