bd3e2f63ee310c75e2eb08154fe00e50e6ef3326
root <rpctropic13@gmail.com>
2024-05-17 16:08:52 +0000

    0123

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*
+ * main - main will interate through the numbers in base 10 starting with 0.
+ * Return: 0
+ */
+int main(void)
+{	
+	int i;
+	for (i = 0; i <= 10, i++)
+	{
+	printf("%d", i);
+	}
+	printf("\n");
+return (0);
+}
