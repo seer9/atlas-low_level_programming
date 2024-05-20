b1066b6882261a187845b6985dccce1adcabdb2a
root <rpctropic13@gmail.com>
2024-05-20 05:11:53 +0000

    sign?

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - This will check what the sign is.
+ *
+ * @n: the needed variable.
+ * Return: 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+	return (1);
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+	return (0);
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+	return (-1);
+	}
+}
