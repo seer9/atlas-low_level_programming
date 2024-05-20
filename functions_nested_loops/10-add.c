5c6f1c761a4a59ff77d5ce4909ad50de6a5c9f43
root <rpctropic13@gmail.com>
2024-05-20 23:06:35 +0000

    sum

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - outputs the sum of i and j
+ *
+ * @i: number 1
+ * @j: number 2
+ * Return: ij = i+j
+ */
+
+int add(int i, int j)
+{
+	int ij
+		ij = i + j;
+	return (ij);
+}
