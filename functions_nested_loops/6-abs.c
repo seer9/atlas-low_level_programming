44dca414c444912d1084563a5b247ca4c6df0213
root <rpctropic13@gmail.com>
2024-05-20 05:52:43 +0000

    abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - absolute value of the variable.
+ *
+ * @o: the variable.
+ *
+ * Return: 0
+ */
+int _abs(int o)
+{
+	if (o < 0)
+	{
+		return (-o);
+	}
+	else
+	{
+		return (o);
+	}
+}
