2a98bab11d05721e3f949f821fdc264e3d5300a5
root <rpctropic13@gmail.com>
2024-05-20 03:33:32 +0000

    abc

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - iterate over the alphabet all lowercase
+ * 
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	int v;
+
+	for (v = 'a'; v <= 'z'; v++)
+	{
+		_putchar(v);
+	}
+	_putchar('\n');
+return (0);
+}
