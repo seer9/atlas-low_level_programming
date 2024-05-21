b2871eaec05a72b8d143b39725498945718e0843
root <rpctropic13@gmail.com>
2024-05-21 04:35:30 +0000

    98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -11,7 +11,7 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		for (int i = n; i <= 98; i++)
+		for (i = n; i <= 98; i++)
 		{
 			if (i == 98)
 			{
@@ -30,7 +30,7 @@ void print_to_98(int n)
 	}
 		else
 		{
-		for (int i = n; i >= 98; i--)
+		for (i = n; i >= 98; i--)
 			{
 			if (i == 98)
 
