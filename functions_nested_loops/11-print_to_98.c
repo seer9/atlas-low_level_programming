5e0af7457320ccd80da76a2ab053d6cdd1fd8c54
root <rpctropic13@gmail.com>
2024-05-21 03:48:32 +0000

    98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * print_to_98 - input for the description.
+ *
+ * @n: variable.
+ */
+void print_to_98(int n);
+{
+	int numbers[] = {0, 98, 111, 81, -10};
+
+	int xray = sizeof(numbers) / sizeof(numbers[0]);
+
+	for (int i = 0; i < size; i++)
+{
+	print_to_98(numbers[i]);
+}
+{
+	int num;
+
+	if (num == 0)
+	{
+		printf("0");
+	}
+	else if (num == 98)
+	{
+		printf("98");
+	}
+	else if (num == 111)
+	{
+		printf("111");
+	}
+	else if (num == 81)
+	{
+		printf("81");
+	}
+	else if (num == -10)
+	{
+		printf("-10");
+	}
+	else
+	{
+		printf("%d", num);
+	}
+	printf("\n");
+}
