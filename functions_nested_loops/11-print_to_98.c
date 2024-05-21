e395b8ba49cdb3ab04f2f6534b12828101f16d13
root <rpctropic13@gmail.com>
2024-05-21 04:27:45 +0000

    98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,1 +1,48 @@
- 
+#include "main.h"
+
+/**
+ *
+ *
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (int i = n; i <= 98; i++)
+		{
+            		if (i == 98)
+			{
+				putchar('9');
+				putchar('8');
+				putchar('\n');
+			break;
+            		}
+			else 
+	    		{
+				putchar(i / 10 + '0'); // First digit
+				putchar(i % 10 + '0'); // Second digit
+				putchar(', ');
+			}
+		}
+	}	
+		else
+		{
+		for (int i = n; i >= 98; i--)
+		{
+		if (i == 98)
+		{
+				putchar('9');
+				putchar('8');
+				putchar('\n');
+			break;
+		}
+			else
+			{
+				putchar(i / 10 + '0'); // First digit
+				putchar(i % 10 + '0'); // Second digit
+				putchar(', ');
+			}
+		}
+	}
+}
