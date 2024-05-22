6a6dc2624bbf99fd63ac813bb4473ad20d56c3bb
root <rpctropic13@gmail.com>
2024-05-22 22:46:17 +0000

    012

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - this is going to iterate through 9.
+ *
+ * @n: the variable going throught the numbers.
+ * Return: void
+ */
+void print_numbers(void)
+{
+int n;
+for (n = 0; n <= 9; n++)
+    printf("%i", n);
+    printf("\n");
+}
