27f5bbf340821bffbd3eb227db6c8d7cc4930775
root <rpctropic13@gmail.com>
2024-05-22 22:32:18 +0000

    012

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - Multiplying 2 variables that are integers.
+ *
+ * @a: first integer.
+ * @b: second integer.
+ * Return: the value of c.
+ */
+int mul(int a, int b)
+{
+int c;
+    c = a * b;
+    return (c);
+
+}
