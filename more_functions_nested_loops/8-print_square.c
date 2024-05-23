8410b8f4c47027327f3d4819184c62c5e80fc7fa
root <rpctropic13@gmail.com>
2024-05-23 22:57:35 +0000

    square

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,1 +1,25 @@
 #include "main.h"
+/**
+ * print_square - creates a square depending on the value held by size.
+ *
+ * @size: the value held by size determines when the program stops.
+ *
+ */
+
+void print_square(int size)
+{
+	int row, col;
+
+	if (size <= 0)
+	{
+		putchar('\n');
+	}
+	for (row = 1; row <= size; row++)
+	{
+		for (col = 1; col <= size; col++)
+		{
+			putchar('#');
+		}
+		putchar('\n');
+	}
+}
