40bdf0ce7ac941f53f77bb53933450de77e98c59
root <rpctropic13@gmail.com>
2024-05-23 01:59:32 +0000

    lines

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_line - this prints a line varying in length.
+ * 
+ * @n: decides how long the line is.
+ */
+
+void print_line(int n)
+{
+	int t;
+
+	if (n < 0)
+	{
+		_putchar ('\n');
+	}
+	else
+		for (; t < n; t++)
+		{
+			_putchar('_');
+		}
+	_putchar('\n');
+}
