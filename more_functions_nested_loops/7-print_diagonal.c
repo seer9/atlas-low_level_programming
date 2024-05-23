222564889eb3e0e2e1d33aaa60c79b7a11ef6d3b
root <rpctropic13@gmail.com>
2024-05-23 06:40:16 +0000

    \

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_diagonal - n is the static location of putchar. i and c are moving to
+ * to the value of n.
+ *
+ * @n: this is the \ key placer variable i believe.
+ */
+
+void print_diagonal(int n)
+{
+	int c = 0;
+
+	while (n > 0)
+	{
+		int i = c;
+
+		while (i > 0)
+		{
+			putchar(' ');
+			i--;
+		}
+		putchar('\\');
+		putchar('\n');
+		c++;
+		n--;
+		if (c < 1)
+		{
+			break;
+		}
+	}
+}
