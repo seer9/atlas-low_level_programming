bb45d025f31ea3338ddd1c0b67cb97cf8a939d99
root <rpctropic13@gmail.com>
2024-05-23 01:23:01 +0000

    10x

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * more_numbers - this is a nested loop to display 1-14 10 times.
+ * Return: void
+ *
+ */
+
+
+
+void more_numbers(void)
+{
+	for (long s = 0; s < 10; s++)
+	{
+		for (long t = 0; t <= 14; t++)
+		{
+			if (t > 9)
+			{
+				putchar((t / 10) + '0');
+			}
+			putchar((t % 10) + '0');
+		}
+		putchar('\n');
+	}
+}
