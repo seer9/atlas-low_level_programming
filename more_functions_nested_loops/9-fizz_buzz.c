81520cce5ee9ebe0f240697803422004cdc03b18
root <rpctropic13@gmail.com>
2024-05-24 16:39:18 +0000

    add

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -8,7 +8,7 @@
  *Return: 0
  */
 
-int main(int count)
+int main(int)
 {
 	for (count = 1; count <= 100; count++)
 	{
@@ -16,18 +16,18 @@ int main(int count)
 		{
 			printf("Fizz");
 		}
-			else if (count % 5 == 0)
-			{	
-			printf("Buzz");
-				}
-				else (count % 3 != 0 && count % 5 != 0)
-				{
-				printf("%d", count);
-				}
-			if (count == 100)
-			{
-			printf("\n");
-			}
+		else if (count % 5 == 0)
+		{	
+		printf("Buzz");
+		}
+		else (count % 3 != 0 && count % 5 != 0)
+		{
+		printf("%d", count);
+		}
+		if (count == 100)
+		{
+		printf("\n");
+		}
 	printf(" ");
 	}
 return (0);
