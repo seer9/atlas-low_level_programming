734a9ce25c44134146c5a1f718c1ddd6a72b89f1
root <rpctropic13@gmail.com>
2024-05-24 03:37:55 +0000

    Buzzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * main - Buzz
+ *
+ *@count: the counter
+ *Return: 0
+ */
+int main(int count)
+{
+	for (count = 1; count <= 100; count++)
+	{
+		if (count % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		if (count % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		if (count % 3 != 0 && count % 5 != 0)
+		{
+			printf("%d", count);
+		}
+		if (count == 100)
+		{
+			printf("\n");
+		}
+		printf(" ");
+	}
+	return (0);
+}
