9f51b8fd3bd8972b7923171b160b7e6a9cb8b962
root <rpctropic13@gmail.com>
2024-05-27 17:10:39 +0000

    fuzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -14,19 +14,18 @@ int main(void)
 		if (count % 3 == 0)
 		{
 			printf("Fizz");
-			printf(" ");
 		}
 		if (count % 5 == 0)
 		{
 			printf("Buzz");
-			printf(" ");
 		}
 		else if (!(count % 3 == 0) && !(count % 5 == 0))
 		{
-			printf("%d ", count);
+			printf("%d", count);
 		}
-		if (count == 100)
-		printf("Buzz");
+	if (count != 100)
+	{
+	printf(" ");
 	}
 return (0);
 }
