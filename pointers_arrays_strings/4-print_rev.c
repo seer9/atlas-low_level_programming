05253ddc2e365641126132a6ca42f4578556ddf2
root <rpctropic13@gmail.com>
2024-05-27 21:09:31 +0000

    rev

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_rev - prints the string character by character
+ * starting at the end and iterating to the beginning.
+ *
+ * @s:the same string as 3 but it gets printed in reverse.
+ */
+void print_rev(char *s)
+{
+	int len = strlen(s);
+
+	for (int l = len - 1; l >= 0; l--)
+	{
+		_putchar(s[l]);
+	}
+	_putchar('\n');
+}
