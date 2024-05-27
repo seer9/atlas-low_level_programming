017f02b6147427a05bbbf4ea2ddc5d3e539d4f68
root <rpctropic13@gmail.com>
2024-05-27 22:22:27 +0000

    rev

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -7,12 +7,12 @@
  */
 void print_rev(char *s)
 {
-	int len = strlen(s);
+	int len = _strlen(s);
 	int i;
 
 	for (i = len - 1; i >= 0; i--)
 	{
-		_putchar(str[i]);
+		_putchar(s[i]);
 	}
 	_putchar('\n');
 }
