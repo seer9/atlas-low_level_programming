dff521eefa0b55f8936a44f754b36ca6f54b518c
root <rpctropic13@gmail.com>
2024-05-27 17:48:57 +0000

    change

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,10 @@
 #include "main.h"
 /**
+ * reset_to_98 - dereference n.
  *
- *
- *
+ * @*n: changing the value of variable n from 402 to 98. 
  */
 void reset_to_98(int *n)
 {
-
-
-
-
-
+	*n = 98;
 }
