b40448d6bd8d2cc5a09b23d7ee2ccc32eebb1006
root <rpctropic13@gmail.com>
2024-05-27 19:16:46 +0000

    langth

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -2,14 +2,16 @@
 /**
  * _strlen - function used to count over a strang.
  * @s: the actual string that is being iterated over.
- *
+ * Return: this simply returns the number of
+ * interations make over the string before the null.
  */
 int _strlen(char *s)
 {
 	int l = 0;
+
 	while (s[l] != '\0')
 	{
 		l++;
 	}
-	return l;
+	return (l);
 }
