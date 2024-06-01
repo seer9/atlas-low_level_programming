4c71d22fe055da4d367983a64397023bfcb70927
root <rpctropic13@gmail.com>
2024-06-01 00:14:04 +0000

    strcat

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -11,10 +11,10 @@ char *_strcat(char *dest, char *src)
 {
 	int dest_len;
 	int i;
-	
-	for (dest_len = 0 ;dest[dest_len] != '\0' ; dest_len++)
+
+	for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
 	{}
-	for (i = 0 ;src[i] != '\0' ; i++)
+	for (i = 0; src[i] != '\0'; i++)
 	{
 		dest[dest_len + i] = src[i];
 	}
