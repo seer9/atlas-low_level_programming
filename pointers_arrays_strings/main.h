1ac4783c8f9c55f8f9675f1e2ad97db91394af29
root <rpctropic13@gmail.com>
2024-06-01 01:40:31 +0000

    strncat

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -5,3 +5,5 @@ int _putchar(char c);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
