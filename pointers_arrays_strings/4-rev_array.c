fc39d7d3353cada586b4a48cd4e7253aad1cc405
root <rpctropic13@gmail.com>
2024-06-02 20:54:47 +0000

    rev array

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /**
- * rev_array - reverses an array of numbers.
+ * reverse_array - reverse an array of numbers.
  *
- * @a: this is the pointer that looks at a in the main function.
- * @n: the max number of elements in the array.
+ * @a: the array
+ * @n: number of elements.
  */
 void reverse_array(int *a, int n)
 {
-    int tpy;
-    int p;
+	int tpy;
+	int p;
 
-    for (p = n - 1; p >= n / 2; p--)
-    {
-        tpy = a[n - 1 - p];
-        a[n - 1 - p] = a[p];
-        a[p] = tpy;
-    }
+	for (p = n - 1; p >= n / 2; p--)
+	{
+		tpy = a[n - 1 - p];
+		a[n - 1 - p] = a[p];
+		a[p] = tpy;
+	}
 }
