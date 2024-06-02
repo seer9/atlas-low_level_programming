11192f29586d2b22c56dea57e90c904e0daaaac9
root <rpctropic13@gmail.com>
2024-06-02 07:47:20 +0000

    strncat

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -2,25 +2,26 @@
 /**
  * _strncat - cndivnvin
  *
- * @dest: kcndk
- * @src: xl
- * Return: dest
+ * @n: placeholder to create an arguement.
+ * @dest: the desired string.
+ * @src: being manipulated.
+ * Return: desired stdout.
  *
  */
 
 
-char *_strncat(char *dest, char *src, int n) 
+char *_strncat(char *dest, char *src, int n)
+
 {
 	int dest_len = 0;
-	
+
 	int i;
 
-
-	while (dest[dest_len] != '\0') 
+	while (dest[dest_len] != '\0')
 	{
 		dest_len++;
 	}
-	for (i = 0; i < n && src[i] != '\0'; i++) 
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[dest_len + i] = src[i];
 	}
