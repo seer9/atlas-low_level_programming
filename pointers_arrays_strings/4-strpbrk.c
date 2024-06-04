5347da890d6df49532b39767bf0f5de838bc8182
root <rpctropic13@gmail.com>
2024-06-04 04:38:47 +0000

    strpbrk

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strpbrk - locates the first occurrence in a string.
+ *
+ * @s: The string to be searched.
+ * @accept: The set of bytes to be searched for.
+ * Return: the matched byte/bytes.
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	int j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (&s[i]);
+			}
+		}
+	}
+}
