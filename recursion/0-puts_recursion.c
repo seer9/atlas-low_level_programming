f0bae38c756b7c5cc5fae9759d37326b144afd55
root <rpctropic13@gmail.com>
2024-06-06 22:08:16 +0000

    puts

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a character from the
+ * string 1 by 1 until the null.
+ *
+ * @s: string being printed.
+ *
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		putchar('\n');
+		return (0);
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
