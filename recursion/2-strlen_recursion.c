3e7ad12de589661ab6db549a785da0b6fa3ccde4
root <rpctropic13@gmail.com>
2024-06-06 23:11:59 +0000

    strlength

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -3,16 +3,17 @@
  * _strlen_recursion - returns the length of the string.
  *
  * @s: the string being counted.
+ * Return: number of turns before null.
  */
 
 int _strlen_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		return 1 + _strlen_recursion(s + 1);
+		return (1 + _strlen_recursion(s + 1));
 	}
 	else
 	{
-		return 0;
+		return (0);
 	}
 }
