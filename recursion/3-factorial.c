b0aa5ec2f13ef796b999696217cbe9b078a99b04
root <rpctropic13@gmail.com>
2024-06-06 23:58:52 +0000

    fact

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -3,7 +3,8 @@
  * factorial - factorial of a given number in main.
  *
  * @n: specified number in main.
- * Return: other then 1 for 0 and 1, it will return the answer from the given number.
+ * Return: other then 1 for 0 and 1, it will return
+ * the answer from the given number.
  */
 
 int factorial(int n)
