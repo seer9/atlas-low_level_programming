ed989bb3fb2787611c8a99b21060650ef9125ef3
root <rpctropic13@gmail.com>
2024-06-06 22:49:39 +0000

    rev

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -5,12 +5,12 @@
  * @s: the string to be reversed.
  */
 
-void _prints_rev_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 	{
 		return;
 	}
-	_prints_rev_recursion(s + 1);
+	_print_rev_recursion(s + 1);
 	_putchar(*s);
 }
