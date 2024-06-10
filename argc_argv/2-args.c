ac8f77d3300490be371b254364d7c6607c56f5c2
root <rpctropic13@gmail.com>
2024-06-10 00:57:40 +0000

    print all

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 /**
+ * main - prints the arguement given to it.
  *
+ * @argc: arguements given
+ * @argv: the object of the arguements.
  *
- *
+ * Return: the arguements given seperated
+ * by new lines.
  *
  */
 int main(int argc, char *argv[])
 {
-        int a;
-	
+	int a;
+
 	for (a = 0; a < argc; a++)
 	{
 		printf("%s\n", argv[a]);
