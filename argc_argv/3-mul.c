f6ee3ff2dd26b9a8927da19d9b38fc7d2b4e0baf
root <rpctropic13@gmail.com>
2024-06-10 01:25:45 +0000

    multi

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - multiply fn and sn but only if
+ * theres 3 arguements
+ *
+ * @argc: arguements being given.
+ * @argv: looks for characters so it needs to be converted
+ * with the atoi function, then multiples prints the converted
+ * arguements.
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int fn;
+	int sn;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	fn = atoi(argv[1]);
+	sn = atoi(argv[2]);
+	printf("%d\n", fn * sn);
+	return (0);
+}
