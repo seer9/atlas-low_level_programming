166d79a17f745bbd8e90aef5a836ad7a25aa2e3a
root <rpctropic13@gmail.com>
2024-06-13 02:17:57 +0000

    spec char

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of characters,
+ * and then initializes it with a specific character.
+ *
+ * @size: input size
+ * @c: initual char
+ *
+ * Return: pointer to the array, or NULL if it fails.
+ */
+
+char *create_array(unsigned int size, char c)
+
+{
+	char *arr = NULL;
+	unsigned int i;
+
+	arr = malloc(sizeof(char) * size);
+
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = c;
+	}
+	return (arr);
+}
---
+++ malloc_free/README.md
@@ -0,0 +1,1 @@
+ In the directory ill be exploring what makes malloc so useful and what i use it for.
