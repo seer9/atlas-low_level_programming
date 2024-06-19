afef2b5a22aabbf2a791e3fb8cc515d924f13913
root <rpctropic13@gmail.com>
2024-06-19 08:20:26 +0000

    print the info passed

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * print_dog - create a variable of type dog
+ * @d: passes the struct dog to the function
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\n", d->name);
+		printf("Age: %.6f\n", d->age);
+		printf("Owner: %s\n", d->owner);
+	}
+}
