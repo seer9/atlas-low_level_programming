69bd2d73680e68567b5368b5c00a87944779d03b
root <rpctropic13@gmail.com>
2024-06-19 08:41:04 +0000

    tweaked header declaration

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -1,21 +1,6 @@
 #include "dog.h"
 #include <stdio.h>
-/**
- * init_dog - initialize a dog struct
- * @d: pointer to dog struct
- * @name: name of dog
- * @age: age of dog
- * @owner: owner of dog
- */
-void init_dog(struct dog *d, char *name, float age, char *owner)
-{
-	if (d != NULL)
-	{
-		d->name = _strdup(name);
-		d->age = age;
-		d->owner = _strdup(owner);
-	}
-}
+#include <stdlib.h>
 /**
  * print_dog - create a variable of type dog
  * @d: passes the struct dog to the function
