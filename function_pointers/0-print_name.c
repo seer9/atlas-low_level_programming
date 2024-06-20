8a79e61ae2812716c7a35885aba8a198223d325c
root <rpctropic13@gmail.com>
2024-06-20 08:16:53 +0000

    changed edge

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
-#include <stdlib.h>
 #include "function_pointers.h"
+
 /**
  * print_name - prints the name.
  * @name: inputs name.
@@ -8,7 +8,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name && f)
+	if (name != NULL || f != NULL)
 		return;
 	f(name);
 }
