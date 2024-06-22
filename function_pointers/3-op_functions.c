5cce75ed02d2481cfa35aaea48d3e3a008709c54
root <rpctropic13@gmail.com>
2024-06-22 16:59:53 +0000

    ops file

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds 2 integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts 2 integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: a minus b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiple 2 integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: a * b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides 2 integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: a / b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - finds the mod of 2 integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the remainder of a % b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
