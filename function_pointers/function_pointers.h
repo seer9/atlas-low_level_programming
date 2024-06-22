f4d94a176ba65837a4d2869c896a41d72edcdac7
root <rpctropic13@gmail.com>
2024-06-22 02:47:14 +0000

    changed header

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
 #ifndef _FUNC_POINTERS_
 #define _FUNC_POINTERS_
 
+#include <stddef.h>
+
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 
