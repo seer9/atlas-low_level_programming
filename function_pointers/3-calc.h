5167ffc3d512526c04d9d479327dbb30044768a4
root <rpctropic13@gmail.com>
2024-06-22 17:16:14 +0000

    update

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -14,3 +14,10 @@ typedef struct op
     char *op;
     int (*f)(int a, int b);
 } op_t;
+/* Ops */
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+#endif
