73db07d7454a46249bdcffe6348c219981b4809d
root <rpctropic13@gmail.com>
2024-06-22 18:08:19 +0000

    final fix

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,11 +1,11 @@
-#include "3-calc.h"
+#include "calc.h"
 
-/**
+ /**
  * main - how the operation is carried out.
- * 
+ *
  * @argc: number of arguments.
  * @argv: the arguement its looking for.
- * 
+ *
  * Return: 0 if 2 positive ints are entered.
  */
 
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
 	int a, b, res;
 
 	char *op;
-	
+
 	if (argc != 4)
 	{
 		printf("Error\n");
@@ -23,12 +23,15 @@ int main(int argc, char *argv[])
 	op = argv[2];
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
-	res = get_op_func(argv[2]);
-	if (op == NULL)
+	/* Get the function pointer */ 
+	int (*func)(int, int) = get_op_func(argv[2]);
+	if (func == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
+	/* Call the function and store the result */
+	res = func(a, b);
 	printf("%d\n", res);
 	return (0);
 }
