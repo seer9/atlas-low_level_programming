eb25c4659e0a1fa8a9b76945baebc8acf0d5bdb1
root <rpctropic13@gmail.com>
2024-06-24 23:16:29 +0000

    sum of all args

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,1 +1,28 @@
 #include "variadic_functions.h"
+
+/**
+ * sum_them_all - sums of all the arguments.
+ *
+ * @n: number of arguments.
+ * Return: sum of all the arguments or 0 if n is 0.
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	if (n == 0)
+	{
+		return (0);
+	}
+	unsigned int i;
+
+	int sum = 0;
+	va_list nums;
+
+	va_start(nums, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(nums, int);
+	}
+	va_end(nums);
+	return (sum);
+}
