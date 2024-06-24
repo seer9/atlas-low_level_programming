25ad4d9a1417b952e3b8aaa534bbcb6bd95d44d7
root <rpctropic13@gmail.com>
2024-06-24 23:23:27 +0000

    sum of all args

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -11,13 +11,12 @@ int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i;
 	int sum = 0;
+	va_list nums;
 
 	if (n == 0)
 	{
 		return (0);
 	}
-	va_list nums;
-
 	va_start(nums, n);
 	for (i = 0; i < n; i++)
 	{
