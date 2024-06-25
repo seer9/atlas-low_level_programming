08cbdb486d2b744a47448eef80b63c0363e58d62
root <rpctropic13@gmail.com>
2024-06-25 01:53:17 +0000

    changed my header

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,5 +1,5 @@
-#ifndef _VAR_FUNCTIONS_
-#define _VAR_FUNCTIONS_
+#ifndef VAR_FUNCTIONS
+#define VAR_FUNCTIONS
 
 #include <stdarg.h>
 #include <stdio.h>
@@ -7,5 +7,6 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif
