8fd380cf11a2b3123bbab06cfb35b7ef18821d21
root <rpctropic13@gmail.com>
2024-07-04 20:54:32 +0000

    changing functions

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,13 +1,28 @@
 #include "lists.h"
 #include <string.h>
 /**
+ * _strlen - function used to count over a strang.
+ * @s: the actual string that is being iterated over.
+ * Return: this simply returns the number of
+ * interations make over the string before the null.
+ */
+int _strlen(char *s)
+{
+	int l = 0;
+
+	while (s[l] != '\0')
+	{
+		l++;
+	}
+	return (l);
+}
+/**
   * add_node - fills memory with a constant byte
   * 
   * @head: names in the list
   * @str: is a string
   * Return: a address
   */
-
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node = malloc(sizeof(list_t));
@@ -21,7 +36,7 @@ list_t *add_node(list_t **head, const char *str)
 		free(new_node);
 		return (NULL);
 	}
-	new_node->len = strlen(str);
+	new_node->len = _strlen(str);
 	new_node->next = *head;
 	*head = new_node;
 	return (new_node);
