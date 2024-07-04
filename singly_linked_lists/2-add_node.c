ff99da313f0dc23c75be427441ccbfc39cefbcbf
root <rpctropic13@gmail.com>
2024-07-04 20:46:58 +0000

    2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,20 +1,28 @@
 #include "list.h"
+#include <string.h>
+/**
+  * add_node - fills memory with a constant byte
+  * 
+  * @head: names in the list
+  * @str: is a string
+  * Return: a address
+  */
 
 list_t *add_node(list_t **head, const char *str)
 {
-list_t *new_node = malloc(sizeof(list_t));
+	list_t *new_node = malloc(sizeof(list_t));
 
-if (new_node == NULL)
-        return (NULL);
+	if (new_node == NULL)
+		return (NULL);
 
-new_node->str = strdup(str);
-  if (new_node->str == NULL)
-  {
-    free(new_node);
-    return (NULL);
-  }
-   new_node->len = strlen(str);
-   new_node->next = *head;
-   *head = new_node;
-   return (new_node);
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->len = strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
 }
