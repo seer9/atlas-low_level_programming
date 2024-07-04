84c5e20c087bdc1606850b25fda7bc34668e2905
root <rpctropic13@gmail.com>
2024-07-04 21:09:38 +0000

    white space

---
+++ singly_linked_lists/_strlen.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * _strlen - function used to count over a strang.
+ * @s: the actual string that is being iterated over.
+ * Return: this simply returns the number of
+ * interations make over the string before the null.
+ */
+int _strlen(char *s)
+{
+	int l = 0;
+
+	while (s[l] != '\0')
+	{
+		l++;
+	}
+	return (l);
+}
+
