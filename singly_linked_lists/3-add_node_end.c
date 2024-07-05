e75515d069a98e85cb42ecbad0d357460d359195
root <rpctropic13@gmail.com>
2024-07-05 08:35:00 +0000

    minor header change

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,47 +1,54 @@
 #include "lists.h"
+
 /**
- * add_node_end - fills memory with a constant byte
- * 
- * @head: is the owner of the dog
+ * str_rec - recursively returns the length of a string.
+ *
+ * @s: input string.
+ * Return: length of string.
+ */
+int str_rec(char *s)
+{
+	if (*s != 0)
+	{
+		return (1 + str_rec(s + 1));
+	}
+	else
+	{
+		return (0);
+	}
+}
+/**
+ * add_node_end - adds a new node to the end of the list
+ *
+ * @head: names in the list
  * @str: is a string
  * Return: a address
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-  list_t *new_node, *temp;
-  int i;
-  new_node = malloc(sizeof(list_t));
-  if (new_node == NULL)
-  {
-    return (NULL);
-  }
-  new_node->str = malloc(sizeof(char) * _strlen(str) + 1);
-  if (new_node->str == NULL)
-  {
-    free(new_node);
-    return (NULL);
-  }
-  i = 0;
-  while (str[i] != '\0')
-    {
-      new_node->str[i] = str[i];
-      i++;
-    }
-  new_node->str[i] = '\0';
-  new_node->len = _strlen(new_node->str);
-  new_node->next = NULL;
-  if (*head == NULL)
-  {
-    *head = new_node;
-  }
-  else
-  {
-    temp = *head;
-    while (temp->next != NULL)
-      {
-        temp = temp->next;
-      }
-    temp->next = new_node;
-  }
-  return (new_node);
-  }
+	list_t *new_n;
+	list_t *temp = *head;
+
+	new_n = malloc(sizeof(list_t));
+	if (new_n == NULL)
+		return (NULL);
+
+	new_n->str = strdup(str);
+	if (!new_n->str)
+	{
+		free(new_n);
+		return (NULL);
+	}
+	new_n->len = str_rec(new_n->str);
+	new_n->next = NULL;
+
+	if (temp)
+	{
+		while (temp->next)
+			temp = temp->next;
+		temp->next = new_n;
+	}
+	else
+		*head = new_n;
+	return (new_n);
+}
--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -24,6 +24,7 @@ size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
+int str_rec(char *s);
 int _strlen(char *s);
 
 #endif
