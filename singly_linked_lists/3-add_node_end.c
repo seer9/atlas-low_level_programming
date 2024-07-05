92777fdb99487fea52052b1f2308da92ac8b1175
Clay <rpctropic13@gmail.com>
2024-07-05 02:15:15 -0500

    white space

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,2 +1,41 @@
 #include "lists.h"
+#include "list.h"
+#include <string.h>
+
+/**
+  * add_node_end - adds a new node to the end of the list
+  *
+  * @head: names in the list
+  * @str: is a string
+  * Return: a address
+  */
+list_t *add_node_end(list_t **head, const char *str)
+{
+    list_t *new_node, *temp;
+
+    new_node = malloc(sizeof(list_t));
+    if (new_node == NULL)
+        return (NULL);
+    new_node->str = strdup(str);
+
+    if (new_node->str == NULL)
+    {
+        free(new_node);
+        return (NULL);
+    }
+    new_node->len = _strlen(new_node->str);
+    new_node->next = NULL;
+    if (*head == NULL)
+    {
+        *head = new_node;
+        return (new_node);
+    }
+    temp = *head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = new_node;
+    return (new_node);
+}
 
