6f1a5a5f59164b39c870175eeff50bcf33eefdc2
root <rpctropic13@gmail.com>
2024-07-05 07:23:00 +0000

    3 and changed header

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -10,31 +10,34 @@
   */
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *new_node, *temp;
+	list_t *new_node, *temp;
 
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-        return (NULL);
-    new_node->str = strdup(str);
+	new_node = malloc(sizeof(list_t));
 
-    if (new_node->str == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
-    new_node->len = _strlen(new_node->str);
-    new_node->next = NULL;
-    if (*head == NULL)
-    {
-        *head = new_node;
-        return (new_node);
-    }
-    temp = *head;
-    while (temp->next != NULL)
-    {
-        temp = temp->next;
-    }
-    temp->next = new_node;
-    return (new_node);
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->len = _strlen(new_node->str);
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+	temp = *head;
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+	}
+	temp->next = new_node;
+	return (new_node);
 }
 
