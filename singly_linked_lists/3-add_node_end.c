c1f3701432e27e667c002b6a52eedee7b0588f63
root <rpctropic13@gmail.com>
2024-07-05 07:45:49 +0000

    new node 3

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,43 +1,47 @@
 #include "lists.h"
-#include <string.h>
-
 /**
-  * add_node_end - adds a new node to the end of the list
-  *
-  * @head: names in the list
-  * @str: is a string
-  * Return: a address
-  */
+ * add_node_end - fills memory with a constant byte
+ * 
+ * @head: is the owner of the dog
+ * @str: is a string
+ * Return: a address
+ */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node, *temp;
-
-	new_node = malloc(sizeof(list_t));
-
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->str = strdup(str);
-
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	new_node->len = _strlen(new_node->str);
-	new_node->next = NULL;
-
-	if (*head == NULL)
-	{
-		*head = new_node;
-		return (new_node);
-	}
-	temp = *head;
-	while (temp->next != NULL)
-	{
-		temp = temp->next;
-	}
-	temp->next = new_node;
-	return (new_node);
-}
-
+  list_t *new_node, *temp;
+  int i;
+  new_node = malloc(sizeof(list_t));
+  if (new_node == NULL)
+  {
+    return (NULL);
+  }
+  new_node->str = malloc(sizeof(const char) * _strlen(str) + 1);
+  if (new_node->str == NULL)
+  {
+    free(new_node);
+    return (NULL);
+  }
+  i = 0;
+  while (str[i] != '\0')
+    {
+      new_node->str[i] = str[i];
+      i++;
+    }
+  new_node->str[i] = '\0';
+  new_node->len = _strlen(str);
+  new_node->next = NULL;
+  if (*head == NULL)
+  {
+    *head = new_node;
+  }
+  else
+  {
+    temp = *head;
+    while (temp->next != NULL)
+      {
+        temp = temp->next;
+      }
+    temp->next = new_node;
+  }
+  return (new_node);
+  }
