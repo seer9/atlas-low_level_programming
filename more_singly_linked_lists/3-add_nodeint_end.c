0071b50cc970530f30e71b059d66fed7349d445a
root <rpctropic13@gmail.com>
2024-07-06 23:43:26 +0000

    minor change to while

---
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+/**
+ * add_nodeint_end - adds a node to the end of the list.
+ *
+ * @head: first element of the list
+ * @n: sets the new node in place
+ * Return: address of the new node
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+    listint_t *new_node, *tmp;
+
+    new_node = malloc(sizeof(listint_t));
+    if (new_node == NULL)
+        return (NULL);
+
+    new_node->n = n;
+    new_node->next = NULL;
+  
+    if (*head == NULL)
+    {
+        *head = new_node;
+        return (new_node);
+    }
+    
+  tmp = *head;
+    
+  while (tmp->next != NULL)
+        tmp = tmp->next;
+    
+  tmp->next = new_node;
+    
+  return (new_node);
+}
