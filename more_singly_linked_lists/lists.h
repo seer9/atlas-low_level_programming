f9000bd64d3b49e7a22a36b0a9dd2781abc46197
root <rpctropic13@gmail.com>
2024-07-06 23:29:55 +0000

    function declaration

--- more_singly_linked_lists/lists.h
+++ more_singly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * struct listint_s - singly linked list
@@ -21,5 +22,6 @@ typedef struct listint_s
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
 
 #endif
