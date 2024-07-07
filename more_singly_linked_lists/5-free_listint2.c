c662b648b788f77788fce813e4e075cc436b52e3
root <rpctropic13@gmail.com>
2024-07-07 00:03:44 +0000

    5

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees the list but sets the head to null.
+ *
+ * @head: beginning of the list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while (*head != NULL)
+	{
+		tmp = *head;
+		*head = (*head)->next;
+		free(tmp);
+	}
+}
