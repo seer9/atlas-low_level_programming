bf352e93255198820b4906ad20f673a26c5cb678
root <rpctropic13@gmail.com>
2024-07-07 00:39:31 +0000

    7

---
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - finds a specific node
+ * in the list
+ *
+ * @head:
+ * @index:
+ *
+ * Return: NULL if it doesnt find it or finds it.
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *tmp;
+	unsigned int i;
+
+	if (head == NULL)
+		return (NULL);
+
+	tmp = head;
+	i = 0;
+
+	while (tmp != NULL)
+	{
+		if (i == index)
+			return (tmp);
+		tmp = tmp->next;
+		i++;
+	}
+	return (NULL);
+}
