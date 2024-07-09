a8657c1e2105bd8569a8845d267a7cbe3af69323
root <rpctropic13@gmail.com>
2024-07-09 03:28:46 +0000

    changed headers

--- file_io/main.h
+++ file_io/main.h
@@ -1,8 +1,12 @@
 #ifndef FILEIO_H
 #define FILEIO_H
 
-#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
