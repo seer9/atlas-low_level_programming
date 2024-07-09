310b62ef21977bb5ee81cdd9ac1b7dab0ac5677e
root <rpctropic13@gmail.com>
2024-07-09 04:16:35 +0000

    1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -6,23 +6,24 @@
  * @text_content: written to the file
  * Return: 1 if successful and -1 on falure
  */
-#include "main.h"
 int create_file(const char *filename, char *text_content)
 {
 	int fd, len;
-	ssize_t bytes_written;
+
+	len = 0;
 
 	if (filename != NULL)
 		return (-1);
-
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd < 0)
 		return (-1);
 	while (text_content && text_content[len])
 		len++;
-	bytes_written = write(fd, text_content, len);
-	if (bytes_written < 0)
+	if (write(fd, text_content, len) < 0)
+	{
+		close(fd);
 		return (-1);
+	}
 	close(fd);
 	return (1);
 }
