5826aa56a754c4c3a858d6ddb6fe2dec3f4c541b
root <rpctropic13@gmail.com>
2024-07-09 00:03:56 +0000

    5-compares 2 numbers

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * flip_bits - counts the number of bits that differ in n     * and m
+ * 
+ * @n: the number
+ * @m: the number to compare to n
+ * Return: the number of bits that differ in n and m
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int flipped;
+	
+	flipped = n ^ m;
+	
+	unsigned int count;
+	
+	count = 0;
+
+	while (flipped > 0)
+	{
+		flipped &= flipped - 1;
+		count++;
+	}
+	return (count);
+}
