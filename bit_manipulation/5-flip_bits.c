863a40cdf850be7112a1a4dd8070aa231fdd07c3
root <rpctropic13@gmail.com>
2024-07-09 00:12:44 +0000

    happy betty

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * flip_bits - counts the number of bits that differ in n     * and m
- * 
+ *
  * @n: the number
  * @m: the number to compare to n
  * Return: the number of bits that differ in n and m
@@ -12,9 +12,9 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned int count;
 
 	flipped = n ^ m;
-	
+
 	count = 0;
-	
+
 	while (flipped > 0)
 	{
 		flipped &= flipped - 1;
