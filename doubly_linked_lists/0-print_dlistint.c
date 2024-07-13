2a83956b887ef92e0288c5fc6bcdebd974969df4
root <rpctropic13@gmail.com>
2024-07-13 17:21:35 +0000

    header and 0

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_dlistint - prints the elements of the list.
+ *
+ * @h: the first element of the list
+ * Return:  number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *current = h;
+
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
