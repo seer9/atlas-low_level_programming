92a832134a59b943dd9eed036617c3f4b33a3ef8
root <rpctropic13@gmail.com>
2024-07-16 03:09:41 +0000

    delete the data

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -14,15 +14,19 @@ void hash_table_delete(hash_table_t *ht)
 	if (ht == NULL)
 		return;
 	for (i = 0; i < ht->size; i++)
-		node = ht->array[i];
-
-	while (node != NULL)
 	{
-		tmp = node;
-		node = node->next;
-		free(tmp->key);
-		free(tmp->value);
-		free(tmp);
+		if (ht->array[i] != NULL)
+		{
+			node = ht->array[i];
+		}
+		while (node != NULL)
+		{
+			tmp = node;
+			node = node->next;
+			free(tmp->key);
+			free(tmp->value);
+			free(tmp);
+		}
 	}
 	free(top->array);
 	free(top);
