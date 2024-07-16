ad00cde14595f1c8b711436775ecdb4b5189da05
root <rpctropic13@gmail.com>
2024-07-16 02:00:52 +0000

    3-sets elements

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -15,7 +15,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
-	copy = strcpy(value);
+	copy = strdup(value);
 	if (copy == NULL)
 		return (0);
 	idx = key_index((const unsigned char *)key, ht->size);
