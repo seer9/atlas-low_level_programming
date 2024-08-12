896a0ef66d343802572580ed584ac41162b8af7b
root <rpctropic13@gmail.com>
2024-08-12 17:21:52 +0000

    betty check

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 /**
- * lin_search - searches for a value in an array
- * 
+ * linear_search - searches for a value in an array
+ *
  * @array: array to search
  * @size: size of the array
  * @value: value to search for
@@ -12,7 +12,7 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
-	if(array == NULL || size < 1)
+	if (array == NULL || size < 1)
 		return (-1);
 
 	for (i = 0; i < size; i++)
