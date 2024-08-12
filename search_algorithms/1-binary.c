e2c8f370c851327eb0307e5a29205b73a0bd4c7f
Clay <rpctropic13@gmail.com>
2024-08-12 17:54:58 +0000

    documentation

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,19 +1,27 @@
 #include "search_algos.h"
-
+/**
+ * binary_search - searches for a value in an array
+ *
+ * @array: array to search
+ * @size: size of the array
+ * @value: value to search for
+ *
+ * Return: index of the value, or -1 if not found
+ */
 int binary_search(int *array, size_t size, int value)
 {
     size_t i, low, high, mid;
 
-    /* If array is NULL or size is less than 1, return -1 */
+    /* if array is NULL or size is less than 1, return -1 */
     if (array == NULL || size < 1)
         return (-1);
-    /* Initialize low and high */
+    /* initialize low and high */
     low = 0;
     high = size - 1;
 
     while (low <= high)
     {
-    /* Print the current section of the array being searched */
+    /* print the half being searched */
         printf("Searching in array: ");
         for (i = low; i <= high; i++)
         {
