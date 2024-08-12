b805772fa0d9abdfcec93bcc7584df0c661714e3
Clay <rpctropic13@gmail.com>
2024-08-12 17:06:57 +0000

    documentation

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,5 +1,13 @@
 #include "search_algos.h"
-
+/**
+ * lin_search - searches for a value in an array
+ * 
+ * @array: array to search
+ * @size: size of the array
+ * @value: value to search for
+ *
+ * Return: index of the value, or -1 if not found
+ */
 int linear_search(int *array, size_t size, int value)
 {
     size_t i;
