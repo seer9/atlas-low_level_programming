5583454d594727127578021c4b102032be8b2127
Clay <rpctropic13@gmail.com>
2024-08-12 06:59:59 +0000

    check

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -4,7 +4,10 @@ int linear_search(int *array, size_t size, int value)
 {
     size_t i;
 
-   for (i = 0; i < size; i++)
+  if(array == NULL || size < 1)
+    return (-1);
+   
+  for (i = 0; i < size; i++)
      {
        if (array[i] == value)
          return (i);
